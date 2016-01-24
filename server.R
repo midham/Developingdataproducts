a479d52d6d353c1c9b8d4e9f4accabdcc303e7a5
midham <midham76@yahoo.com>
2016-01-24 20:41:22 +0800

    Module 9 Assignment

---
+++ server.R
@@ -0,0 +1,199 @@
+###
+# File     : server.R
+# Author   : midham
+# Context  : shiny application
+#   AppName: Simple Analytics of Japanese Population, 1970 - 2010
+#   For    : Coursera Data Scientist Specialization
+#   For    : Module 9 - Developing Data Products
+#   Date   : 23 Jan 2016
+###
+
+library(shiny)
+#library(rCharts)
+library(ggplot2)
+
+# load primary file that process the datafile
+source("reloadData.R")
+
+# line only used during debugging. 
+#options(shiny.error=browser) 
+
+## UI Layout Brief
+# UI has a Top Navigation Bar, with 4 Navigation Panel : 
+# 1. Population View by Prefecture (Prefecture View)
+# 2. Age Tracking by Prefecture (Age Tracking)
+# 3. Age Group Statistics on Census Year (Age Group View)
+# 4. Age Group Evolution in Time (Age Group Evolution)
+# 5. Raw Data View (Table View)
+# 6. About Page (About)
+## --
+
+shinyServer(function(input, output) {
+
+  # [     ]  Area Plot of Population Proportion Distribution over time each selected Prefecture 
+  #          at selected prefecture on Navigation Panel (Prefecture View)
+  output$chart100pct_pref <- renderPlot({
+    minyear <- input$timeframe[1]
+    maxyear <- input$timeframe[2]
+    pref <- input$pref
+    x <- PrefAgePop(minyear=minyear, maxyear=maxyear, pref = pref)
+    dx <- dim(x)
+    p<-ggplot(x, aes(Year, PopPct))+geom_area()+aes(fill=AgeGroup)
+    p<-p+theme(legend.position="right",panel.background=element_rect(fill="white"))+
+      labs(y="Population, %", 
+           title=paste("Evolution of Age Group Distribution in", getPrefName(pref)))+
+      guides(fill=guide_legend(title="Age Group", reverse=TRUE))
+    return(p)
+  })
+  
+  # [     ]  Area Plot of Population Distribution over time each selected Prefecture
+  #          at selected prefecture on Navigation Panel (Prefecture View)
+  output$chart_pref <- renderPlot({
+    minyear <- input$timeframe[1]
+    maxyear <- input$timeframe[2]
+    pref <- input$pref
+    agegrp <- input$agegroup
+    x <- PrefAgePop(minyear=minyear, maxyear=maxyear, pref = pref)
+    dx <- dim(x)
+    p<-ggplot(x, aes(Year, Population/1000))+geom_area()+aes(fill=AgeGroup)
+    p<-p+theme(legend.position="right",panel.background=element_rect(fill="white"))+
+      labs(y="Population (millions)", title=paste("Evolution of Age Group Distribution in", getPrefName(pref)))+
+      guides(fill=guide_legend(title="Age Group", reverse=TRUE))
+    return(p)
+  })
+  
+  # [      ] Plot Population Proportion Change over time Group by Birth Year and initial age group
+  #          on Navigation Panel (Age Tracking)
+  output$AgeEvolutionPct <- renderPlot({
+    minyear <- input$timeframe2[1]
+    maxyear <- input$timeframe2[2] 
+    pref <- input$pref2
+    paste("Age Evo : ", pref)
+    x <- getBornYearStat(minyear, maxyear, pref)    
+    r <- ggplot(x, aes(MinAge,PopPct))+geom_line()+aes(colour=factor(BornYear))+
+      guides(col=guide_legend(ncol=6, title="Year Born", reverse=TRUE))
+    r<-r+theme(panel.background=element_rect(fill="white"))
+    r<-r+labs(x="Age during Census",y="% of Population", 
+              title=paste("Age vs Population Proportion tracking by Year of Birth in", 
+                          getPrefName(pref)))
+    
+    return (r)
+  })
+  
+  # [      ] Plot Population Change over time Group by Birth Year and initial age group
+  #          on Navigation Panel (Age Tracking)
+  output$AgeEvolution <- renderPlot({
+    minyear <- input$timeframe2[1]
+    maxyear <- input$timeframe2[2] 
+    pref <- input$pref2
+    x <- getBornYearStat(minyear,maxyear,pref)   
+    
+    r <- ggplot(x, aes(MinAge,Population/1000))+geom_line()+geom_point()+
+      aes(colour=factor(BornYear))+
+      guides(col=guide_legend(ncol=6, title="Year Born", reverse=TRUE))
+    r<-r+theme(panel.background=element_rect(fill="white"),
+               legend.key = element_rect(fill = "white"))
+    r<-r+labs(x="Age during Census",y="Total Population (millions)", 
+              title=paste("Age vs Population tracking by Year of Birth in", 
+                          getPrefName(pref)))
+    return (r)
+  })
+
+  # [      ] Plot Population Proportion Distribution over time for selected Age Group
+  #          on Navigation Panel (Age Group View)
+  output$showAgeGroupBoxPct <- renderPlot({
+    yr = input$agyear
+    qstat <- getQuantileStat(yr)
+    g <- ggplot(qstat, aes(AgeGroup, PopPct))+geom_boxplot()+xlab("Age Group")+
+      ylab("Population, %")
+    g<-g+labs(title=paste("Population Statistics across Japan, Year = ", yr))
+    g<-g+theme(panel.background=element_rect(fill="white"), legend.position="none")+
+      aes(fill=AgeGroup)
+    g<-g+theme(axis.text.x=element_text(angle=90))
+    return (g)
+  })
+  
+  # [      ] Plot Population Proportion Distribution for each age group for selected Census Year
+  #          on Navigation Panel (Age Group Evolution)
+  output$showAgeGroupEvolutionBoxPct <- renderPlot({
+    yr = getAgeGroupName(input$ag)
+    qstat <- getAgeQuantileStat(yr)
+    g <- ggplot(qstat, aes(as.factor(Year), PopPct))+geom_boxplot()+xlab("Year")+
+      ylab("Population, %")
+    g<-g+labs(title=paste("Age Group",yr,"Population across Japan"))
+    g<-g+theme(panel.background=element_rect(fill="white"), legend.position="none")+
+      aes(fill=as.factor(Year))
+    g<-g+theme(axis.text.x=element_text(angle=90))
+    return (g)
+  })
+  
+  # [      ] Show data table interactivelyon Navigation Panel (Table View)
+  output$dispStatTable <- renderDataTable(jpstat)
+  
+  # [unused] Plot Population Distribution over time for selected Age Group
+  #          on Navigation Panel (--unused--)
+  output$showAgeGroupEvolutionBox <- renderPlot({
+    yr = getAgeGroupName(input$ag)
+    qstat <- getAgeQuantileStat(yr)
+    g <- ggplot(qstat, aes(as.factor(Year), Population))+geom_boxplot()+xlab("Year")+
+      ylab("Population, (million)")
+    g<-g+labs(title=paste("Age Group",yr,"Population across Japan"))
+    g<-g+theme(panel.background=element_rect(fill="white"), legend.position="none")+
+      aes(fill=as.factor(Year))
+    g<-g+theme(axis.text.x=element_text(angle=90))
+    return (g)
+  })
+
+  # [unused] Violin Plot of Population Proportion Distribution over time for selected Age Group
+  #          on Navigation Panel (--unused--)
+  output$showAgeGroupViolin <- renderPlot({
+    yr = input$timeframe[1]
+    qstat <- getQuantileStat(yr)
+    g <- ggplot(qstat, aes(AgeGroup, PopPct))+geom_violin()+xlab("Age Group")+
+      ylab("Population, %")
+    g<-g+labs(title=paste("Population Statistics across Japan, Year = ", yr))
+    g<-g+theme(panel.background=element_rect(fill="white"), legend.position="none")+
+      aes(fill=AgeGroup)
+    g<-g+theme(axis.text.x=element_text(angle=90))
+    return (g)
+  })
+  
+  # [unused] display the timerange selected on Navigation Panel (--unused--)
+  output$timerange <- renderText({
+    paste("Input Year from ", input$timeframe[1], " to", input$timeframe[2])
+  })
+  
+  # [unused] display the Prefecture selected on Navigation Panel (--unused--) 
+  output$prefecture_selected <- renderText({
+    paste("Prefecture Selected : ", preflist$Prefecture[preflist$PID==input$pref])
+  })
+  
+  # [unused] display selected Age Range on Navigation Panel (--unused--)
+  output$age_selected <- renderText({
+    minage<-(as.integer(input$agegroup)-1)*5
+    paste("Age Group : From ", agelist$MinAge[agelist$MinAge == minage], 
+          "years old to", agelist$MaxAge[agelist$MinAge ==minage], "years old (",
+          agelist$AgeGroup[agelist$MinAge == minage], ")")
+  })
+  
+  # [unused] display selected Age Group on Navigation Panel (--unused--)
+  output$agegroup_selected <- renderText({
+    minage<-(as.integer(input$agegroup)-1)*5
+    paste("Age Group : ", agelist$AgeGroup[agelist$MinAge == minage])
+  })
+  
+  # [unused] Plot Population Distribution for each age group for selected Census Year
+  #          on Navigation Panel (--unused--)
+  output$showAgeGroupBox <- renderPlot({
+    yr = input$agyear
+    qstat <- getQuantileStat(yr)
+    g <- ggplot(qstat, aes(AgeGroup, Population))+geom_boxplot()+xlab("Age Group")+
+      ylab("Population, (million)")
+    g<-g+labs(title=paste("Population Statistics across Japan, Year = ", yr))
+    g<-g+theme(panel.background=element_rect(fill="white"), legend.position="none")+
+      aes(fill=AgeGroup)
+    g<-g+theme(axis.text.x=element_text(angle=90))
+    return (g)
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,169 @@
+###
+# File     : ui.R
+# Author   : midham
+# Context  : shiny application
+#   AppName: Simple Analytics of Japanese Population, 1970 - 2010
+#   For    : Coursera Data Scientist Specialization
+#   For    : Module 9 - Developing Data Products
+#   Date   : 23 Jan 2016
+###
+
+# The user-interface definition of the Shiny web app.
+library(shiny)
+#library(rCharts)
+library(dplyr)
+
+source("reloadData.R")
+
+# line only used during debugging. 
+#options(shiny.error=browser) 
+
+## UI Layout Brief
+# UI has a Top Navigation Bar, with 4 Navigation Panel : 
+# 1. Population View by Prefecture (Prefecture View)
+# 2. Age Tracking by Prefecture (Age Tracking)
+# 3. Age Group Statistics on Census Year (Age Group View)
+# 4. Age Group Evolution in Time (Age Group Evolution)
+# 5. Raw Data View (Table View)
+# 6. About Page (About)
+## --
+
+
+shinyUI(
+  fluidPage(
+    titlePanel("Visualization of Japan Population Estimates"),
+    navbarPage(
+      "Japan",
+      tabPanel(
+        "Population View", 
+        sidebarLayout(
+          position = "right",
+          sidebarPanel(
+            "Select Parametric Value : ",
+            sliderInput(
+              "timeframe",
+              "Census Year",
+              min = 1970, 
+              max = 2010, 
+              value = c(1980, 2005)
+            ), #sliderInput.timeframe
+            selectInput(
+              "pref", 
+              label = h3("Select Prefecture"), 
+              choices = listPref(),
+              selected = 0
+            ) #selectInput.pref
+          ), #sideBarPanel.control
+          mainPanel(
+            h4("Background"),
+            p(paste0("The plots below shows the total population evolution in time for each age group.",
+                     "The age group is in the format a{xx}t{yy} where it is read as the age group between {xx} and {yyy}.",
+                     "You can use the control panel on the right to examine the population of each Prefecture during the desired time range.")),
+            h4("Total population evolution in time by age group"),
+            plotOutput("chart_pref"),
+            h4("Total population proportion evolution in time by age group"),
+            plotOutput("chart100pct_pref")
+          )#mainpanel
+        )#SiteBarLayout.right
+      ),#Tabpanel.tab1
+      tabPanel( 
+        "Age tracking", 
+        sidebarLayout(
+          position = "right",
+          sidebarPanel(
+            "Select Parametric Value : ",
+            sliderInput(
+              "timeframe2", 
+              "Census Year",
+              min = 1970, 
+              max = 2010, 
+              value = c(1980, 2005)
+            ), #sliderInput.timeframe2
+            selectInput(
+              "pref2", 
+              label = h3("Select Prefecture"), 
+              choices = listPref(),
+              selected = 0
+            )
+          ), #sideBarPanel.control
+          mainPanel(
+            h1("Population tracking in Time"),
+            p("The population are grouped by their birth year and their age. The birth year is within +/- 5 years since the census age rage is 5 years."),
+            p("Each line shows a group of population born the same year, and how the group size grows or shrink in time."),
+            p("An increasing slope shows net immigration while decreasing slope shows net decline in population due to death or emigration."),
+            p("You can visualize at what age the emigration or immigration happens and which prefecture gains or lose most population."),
+            h4("View by population"),
+            plotOutput("AgeEvolution"),
+            h4("View by percentage of population of that age group compared to overall  population during the census year"),
+            plotOutput("AgeEvolutionPct")
+          )#mainpanel
+        )#SiteBarLayout.right
+      ),#Tabpanel.tab2
+      tabPanel( 
+        "Age Group View", 
+        sidebarLayout(
+          position = "right",
+          sidebarPanel(
+            "Select Parametric Value : ",
+            selectInput(
+              "agyear", 
+              label = h3("Census Year"), 
+              choices = yearlist,
+              selected = 1980
+            )
+          ), #sideBarPanel.control
+          mainPanel(
+            p("The graph below shows how the population proportion for each age group for the selected year is distributed between the different prefectures."),
+            plotOutput("showAgeGroupBoxPct")
+          )#mainpanel
+        )#SiteBarLayout
+      ),#Tabpanel.tab3
+      tabPanel( 
+        "Age Group Evolution", 
+        sidebarLayout(
+          position = "right",
+          sidebarPanel(
+            "Select Parametric Value : ",
+            selectInput(
+              "ag", 
+              label = h3("AgeGroup"), 
+              choices = listAgeGroup(),
+              selected = 1
+            )
+          ), #sideBarPanel.control
+          mainPanel(
+            p("The graph below shows how the population proportion of the selected age group changes over time."),
+            p("This will show in a much clearer way on which age group is increasing and which are decreasing over time."),
+            p("You will see that the birth rate is decreasing while the longevity is increasing."),
+            plotOutput("showAgeGroupEvolutionBoxPct")
+          )#mainpanel
+        )#SiteBarLayout
+      ),#Tabpanel.tab4
+      tabPanel( 
+        "Table View", 
+        mainPanel(
+          p("You can use the fields at the bottom of this table to filter data."),
+          fluidRow(
+            column(10,
+                   dataTableOutput('dispStatTable')
+            ) # column
+          ) #fluid row
+        ) #main
+      ),#Tabpanel.tab5
+      tabPanel( 
+        "About", 
+        mainPanel(
+          h2("Data source"),
+          p("Census data from 1970 to 2010 is downloaded from "),
+          a("http://www.stat.go.jp/english/data/jinsui/2.htm"),
+          h2("Preprocessing"),
+          p("The data from multiple Excel spreadsheets are extracted and processed using the R-script reshape_jap.R and reshape_jap2.R."),
+          p("The shinyApp only use preprocessed file to speed up the operations."),
+          h2("Slides"),
+          p("The pitching slides can be found"),
+          a("http://smjb.github.io/devdata_pitch")
+        ) #main
+      )#Tabpanel.tab5
+    ) #navPanel
+  )#fluidpage
+)#shiny
